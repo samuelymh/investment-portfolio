8ac4806fc02cfe6e681af1caee530678b88e2b19
Samuel <samuel.yau2412@gmail.com>
2023-01-22 15:31:49 +0800

    chg: Refactored countdown clock to next market holiday

--- src/Components/Clock/Clock.tsx
+++ src/Components/Clock/Clock.tsx
@@ -24,23 +24,20 @@ const Clock: React.FC = () => {
   }
 
   const updateClock = setInterval(() => {
-    let countdownDate = new Date(data[2023][2]).getTime();
-    let distance = countdownDate - new Date().getTime();
+    const countdownDate = new Date(data[2023][2]).getTime();
+    const distance = countdownDate - new Date().getTime();
 
-    let days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-    // Display result in clock element
-    // If multiple days, display days.
-    // Else if multiple hours, display HH:MM:SS.
-    if (days > 0) {
-      setTime(days + " days till next market holiday");
-    } else if (hours > 0) {
-      setTime(hours + "H " + minutes + "M " + seconds + "S");
-    } else if (minutes > 0) {
-      setTime(minutes + "M " + seconds + "S");
+    const time = [days, hours, minutes, seconds]
+      .map(unit => unit.toLocaleString(undefined, {minimumIntegerDigits: 2, useGrouping:false}))
+
+    // If countdown not finished, update the clock.
+    if (seconds >= 0) {
+      setTime(time[0] + ' : ' + time[1] + " : " + time[2] + " : " + time[3])
     }
 
     // If countdown finished, convert to text.
@@ -55,7 +52,7 @@ const Clock: React.FC = () => {
       className='text-black inline-block h-full text-base font-normal'
       onClick={() => printData()}>
       <BellOutlined className='align-middle'/>  
-      <span className='align-middle clockValue'></span>
+      <span className='align-middle clockValue ml-1'></span>
     </div>
   );
 };
