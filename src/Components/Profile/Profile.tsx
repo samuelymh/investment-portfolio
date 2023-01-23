877b21291d6f966d811d0cb6e82f77168f5cfcf7
Samuel <samuel.yau2412@gmail.com>
2023-01-24 01:05:51 +0800

    chg: Refactored Profile component to use ant design system

--- src/Components/Profile/Profile.tsx
+++ src/Components/Profile/Profile.tsx
@@ -1,107 +1,55 @@
 import React from 'react';
 
 // Ant Design components
-import { Select } from 'antd';
-import { UserOutlined } from '@ant-design/icons';
-
-// MUI components
-import Box from "@mui/material/Box";
-import Avatar from "@mui/material/Avatar";
-import Menu from "@mui/material/Menu";
-import MenuItem from "@mui/material/MenuItem";
-import ListItemIcon from "@mui/material/ListItemIcon";
-import IconButton from "@mui/material/IconButton";
-import Settings from "@mui/icons-material/Settings";
-import Logout from "@mui/icons-material/Logout";
+import { Select, Dropdown, Avatar } from 'antd';
+import type { MenuProps } from 'antd';
+import {
+  UserOutlined,
+  SettingOutlined,
+  LogoutOutlined
+} from "@ant-design/icons";
 
 const Profile: React.FC = () => {
-
-  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
-  const open = Boolean(anchorEl);
-  const handleClick = (event: React.MouseEvent<HTMLElement>) => {
-    setAnchorEl(event.currentTarget);
-  };
-  const handleClose = () => {
-    setAnchorEl(null);
-  };
-
   const handleChange = (value: string) => {
     console.log(`selected ${value}`);
   };
 
+  const items: MenuProps["items"] = [
+    {
+      key: "1",
+      label: "Settings",
+      icon: <SettingOutlined />
+    },
+    {
+      key: "2",
+      label: "Logout",
+      icon: <LogoutOutlined />
+    }
+  ];
+
   return (
-    <div className='h-full'>
+    <div className='h-full flex items-center'>
       {/* Dropdown to select portfolio */}
       <Select
-        className='mr-2 !w-32'
+        className='mr-2 !w-32 align-middle'
         defaultValue="Portfolio 1"
         options={[{ value: 'portfolio1', label: 'Portfolio 1'},
                   { value: 'portfolio2', label: 'Portfolio 2'}]}
         onChange={handleChange}
       />
 
-      <div className='inline-block'>
+      <div>
         {/* Dropdown to access settings and logout button */}
-        <Box sx={{ display: "flex", alignItems: "center", textAlign: "center" }}>
-          <IconButton
-            onClick={handleClick}
-            size="small"
-            sx={{ ml: 2 }}
-            aria-controls={open ? "account-menu" : undefined}
-            aria-haspopup="true"
-            aria-expanded={open ? "true" : undefined}
-          >
-            <Avatar sx={{ width: 36, height: 36 }}><UserOutlined /></Avatar>
-          </IconButton>
-        </Box>
-        <Menu
-          anchorEl={anchorEl}
-          id="account-menu"
-          open={open}
-          onClose={handleClose}
-          onClick={handleClose}
-          PaperProps={{
-            elevation: 0,
-            sx: {
-              overflow: "visible",
-              filter: "drop-shadow(0px 2px 8px rgba(0,0,0,0.32))",
-              mt: 1.5,
-              "& .MuiAvatar-root": {
-                width: 32,
-                height: 32,
-                ml: -0.5,
-                mr: 1
-              },
-              "&:before": {
-                content: '""',
-                display: "block",
-                position: "absolute",
-                top: 0,
-                right: 14,
-                width: 10,
-                height: 10,
-                bgcolor: "background.paper",
-                transform: "translateY(-50%) rotate(45deg)",
-                zIndex: 0
-              }
-            }
-          }}
-          transformOrigin={{ horizontal: "right", vertical: "top" }}
-          anchorOrigin={{ horizontal: "right", vertical: "bottom" }}
+        <Dropdown menu={{ items }} placement="bottom" arrow
+          className=''
         >
-          <MenuItem>
-            <ListItemIcon>
-              <Settings fontSize="small" />
-            </ListItemIcon>
-            Settings
-          </MenuItem>
-          <MenuItem>
-            <ListItemIcon>
-              <Logout fontSize="small" />
-            </ListItemIcon>
-            Logout
-          </MenuItem>
-        </Menu>
+          <div>
+            <Avatar
+              className='flex justify-center items-center'
+              size="large" 
+              icon={<UserOutlined className=''/>} />
+          </div>
+        </Dropdown>
       </div>
     </div>
   );
