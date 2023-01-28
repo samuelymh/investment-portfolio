ac9c05d262552e9e351344db8d100b1a0dcb9f6b
Samuel <samuel.yau2412@gmail.com>
2023-01-29 04:20:32 +0800

    feat: Implemented top data containers on dashboard page

--- src/Components/Dashboard/Dashboard.tsx
+++ src/Components/Dashboard/Dashboard.tsx
@@ -1,9 +1,67 @@
 import React from 'react';
 
+import {
+  CaretUpOutlined,
+  CaretDownOutlined,
+} from '@ant-design/icons';
+
 const Dashboard: React.FC<{ classes: string }> = ({ classes }) => {
+
+  const classNameContainer = "box-border border-black border-2 p-3 w-1/5";
+
   return (
     <div className={classes}>
-      <h1 className='text-center'>Dashboard</h1>
+      <h1 className='text-center'>Dashboard</h1><br/>
+
+      <div className='flex justify-around px-20'>
+        {/* Displays NLV and total invested. */}
+        <div className={classNameContainer}>
+          <p className='text-base'>Value</p>
+          <p className='text-xl font-medium'>$167,245.32</p>
+          <p className="text-lg">$132,784.52 invested</p>
+        </div>
+
+        {/* Displays cash remaining in both number and percentage. */}
+        <div className={classNameContainer}>
+          <p>Cash Remaining</p>
+          <p className='text-xl font-medium'>$34,460.80</p>
+          <p className='text-lg'>20.60% unallocated</p>
+        </div>
+
+        {/* Displays YTD and day's returns in number and percentage.*/}
+        <div className={classNameContainer}>
+          <p>Returns</p>
+          <p className='text-xl font-medium'>
+            +$23,042
+            <span className='text-base text-green-600'>
+              <CaretUpOutlined className='align-middle pb-1'/>
+              17.5%
+            </span>
+          </p>
+          <p className='text-lg'>
+            -$1,328
+            <span className='text-base text-red-600'>
+              <CaretDownOutlined className='align-middle pb-1'/>
+              0.8%
+            </span>
+          </p>
+        </div>
+
+        {/* Displays yield(%) relative to total invested and in number.*/}
+        <div className={classNameContainer}>
+          <p>Dividends</p>
+          <p className='text-xl font-medium'>
+            1.8%
+            <span className='text-base text-green-600'>
+              <CaretUpOutlined className='align-middle pb-1'/>
+              0.2%
+            </span>
+          </p>
+          <p className='text-lg'>$2,384.32</p>
+        </div>
+      </div>
+      <br/>
+
       {/* Testing filler content */}
       <div style={{ padding: 24, textAlign: 'center', background: "pink" }}>
         <p>long content</p>
